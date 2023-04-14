55840f5199df2217fcf1a0c05c55b886d2f3428c
Yuuki <928196210@qq.com>
2023-04-14 14:53:43 +0800

    init

---
+++ src/main/java/com/nqff/drms/DrmsApplication.java
@@ -0,0 +1,13 @@
+package com.nqff.drms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrmsApplication.class, args);
+	}
+
+}
