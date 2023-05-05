c319ea898dc8ce16fe32da99441efdda5504d6c0
shenqingyang <1194346674@qq.com>
2023-05-05 19:36:01 +0800

    完善类别

--- src/main/java/com/nqff/drms/controller/CategoryController.java
+++ src/main/java/com/nqff/drms/controller/CategoryController.java
@@ -2,11 +2,8 @@ package com.nqff.drms.controller;
 
         import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
         import com.nqff.drms.pojo.Category;
-        import com.nqff.drms.pojo.Document;
         import com.nqff.drms.pojo.Project;
-        import com.nqff.drms.pojo.User;
         import com.nqff.drms.service.CategoryService;
-        import com.nqff.drms.service.ProjectService;
         import com.nqff.drms.utils.Result;
         import io.swagger.v3.oas.annotations.Operation;
         import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -23,15 +20,36 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @Operation(summary = "根据 id 获取指定资料信息", security = {@SecurityRequirement(name = "Authorization")})
+    @Operation(summary = "根据用户id和项目id获取指定类别信息", security = {@SecurityRequirement(name = "Authorization")})
     @GetMapping(path = "/{uid}&{pid}")
     public Result getDocumentById(@PathVariable Integer uid ,@PathVariable Integer pid) {
         System.out.println(uid + " " + pid);
-        List<Category> categories = categoryService.testFunc(uid,pid);
+        List<Category> categories = categoryService.selectCategoryByUserIdAndProjectId(uid,pid);
         System.out.println(categories.size());
         if (categories == null) {
             return Result.FAIL("not found", null);
         }
         return Result.SUCCESS(categories);
     }
+    @Operation(summary = "根据 id 删除类别", security = {@SecurityRequirement(name = "Authorization")})
+    @DeleteMapping(path = "/{id}")
+    public Result deleteProjectById(@PathVariable Integer id) {
+        categoryService.removeById(id);
+        return Result.SUCCESS(null);
+    }
+
+    @Operation(summary = "更新类别信息", security = {@SecurityRequirement(name = "Authorization")})
+    @PutMapping()
+    public Result updateProjectInfo(@RequestBody Category category) {
+        categoryService.updateById(category);
+        return Result.SUCCESS(null);
+    }
+
+    @Operation(summary = "新增类别", security = {@SecurityRequirement(name = "Authorization")})
+    @PostMapping(path = "/create")
+    public Result createNewProject(@RequestBody Category category) {
+        categoryService.insertCategory(category);
+        return Result.SUCCESS(null);
+    }
+
 }
