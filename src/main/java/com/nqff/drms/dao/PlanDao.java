360e3bf3ccc35db45d13248e7f2dc90237d8daf1
shenqingyang <1194346674@qq.com>
2023-05-15 17:31:34 +0800

    --

--- src/main/java/com/nqff/drms/dao/PlanDao.java
+++ src/main/java/com/nqff/drms/dao/PlanDao.java
@@ -9,11 +9,6 @@ import java.util.List;
 @Mapper
 public interface PlanDao extends BaseMapper<Plan> {
     @Select("select * from `plan` where id = #{id} and deleted = 0")
-    @Results({
-            @Result(column = "id",property = "id"),
-            @Result(column = "id",property = "documents",javaType = List.class,
-                    many = @Many(select = "com.nqff.drms.dao.DocumentDao.selectByPlanId"))
-    })
 Plan selectPlanById(int id);
 
     @Select("select * from `plan` where subproject_id = #{subproject_id} and deleted = 0")
